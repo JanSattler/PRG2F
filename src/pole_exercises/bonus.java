8847da4f6a468d1eb75bf084e843f3df9cd2f06f
pohrebacek <petrjan872@gmail.com>
2023-11-12 10:01:48 +0100

    more pole exercises

--- src/pole_exercises/bonus.java
+++ src/pole_exercises/bonus.java
@@ -6,22 +6,25 @@ import java.util.Scanner;
 public class bonus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] pickedNumbers = new int[10];
-        int[] guesses = new int[5];
+        //int[] pickedNumbers = new int[10];
+        //int[] guesses = new int[5];
+        int[] pickedNumbers = {30, 3, 30, 1, 13, 16, 15, 13, 15, 4};
+        int[] guesses = {15, 25, 13, 19, 21};
 
-        for (int i = 0; i < pickedNumbers.length; i++) {
-            pickedNumbers[i] = (int) (Math.random() * 30 + 1);
-        }
+        //for (int i = 0; i < pickedNumbers.length; i++) {
+        //    pickedNumbers[i] = (int) (Math.random() * 30 + 1);
+        //}
 
-        for (int i = 0; i < guesses.length; i++) {
-            System.out.println("Zadej tip: ");
-            guesses[i] = sc.nextInt();
-        }
+        //for (int i = 0; i < guesses.length; i++) {
+        //    System.out.println("Zadej tip: ");
+        //    guesses[i] = sc.nextInt();
+        //}
 
 
-        int idk = 0;
+        //int idk = 0;
         System.out.print("Picked numbers:");
         System.out.println(Arrays.toString(pickedNumbers));
+        System.out.println(Arrays.toString(guesses));
         System.out.println("Guesses: ");
 
         //for (int i = 0; i < pickedNumbers.length; i++) {
@@ -34,18 +37,37 @@ public class bonus {
         //    }
         //}
 
+        //int count = 0;
+        //for (int i = 0; i < pickedNumbers.length; i++) {
+        //    if (pickedNumbers[i] == guesses[i]) {
+        //        System.err.println(guesses[i]);
+        //    } else {
+        //        System.out.println(guesses[i]);
+        //    }
+        //    count++;
+        //    if (count == guesses.length) {
+        //        break;
+        //    }
+        //}
+
+
         int count = 0;
-        for (int i = 0; i < pickedNumbers.length; i++) {
-            if (pickedNumbers[i] == guesses[i]) {
-                System.err.println(guesses[i]);
-            } else {
+        for (int i = 0; i < guesses.length; i++) {
+            boolean idkmore = false;
+            for (int j = 0; j < pickedNumbers.length; j++) {
+                if (guesses[i] == pickedNumbers[j]) {
+                    System.err.println(guesses[i]);
+                    count += 1;
+                    idkmore = true;
+                    break;
+                }
+            }
+            if (!idkmore) {
                 System.out.println(guesses[i]);
             }
-            count++;
-            if (count == guesses.length) {
-                break;
-            }
         }
+        System.out.println("Soutěžící správně tipl čísel: "+count);
+
 
         //for (int i = 0; i < pickedNumbers.length; i++) {
         //    for (int j = 0; j < guesses.length; j++) {
