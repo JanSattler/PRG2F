faba4168e428986bf185d8a3dcbf5aa580e32ba2
pohrebacek <petrjan872@gmail.com>
2023-12-09 16:12:36 +0100

    more exercises

---
+++ src/String_exercises/morseovka.java
@@ -0,0 +1,17 @@
+package String_exercises;
+
+import java.util.Scanner;
+
+public class morseovka {
+    Scanner sc = new Scanner(System.in);
+    String[] letters = {"A .-", "B -...", "C -.-.", "D -..", "E .",
+            "F ..-.", "G --.", "H ....", "I ..", "J .---",
+            "K -.-", "L .-..", "M --", "N -.", "O ---",
+            "P .--.", "Q --.-", "R .-.", "S ...", "T -",
+            "U ..-", "V ...-", "W .--", "X -..-", "Y -.--",
+            "Z --.."};
+    String input = sc.nextLine();
+    String finalWord = "";
+
+
+}
