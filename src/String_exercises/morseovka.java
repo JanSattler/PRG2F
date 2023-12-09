4bad6f04b2690f9edef8f41cef6a1cf03a08383a
pohrebacek <petrjan872@gmail.com>
2023-12-09 16:28:09 +0100

    more exercises

--- src/String_exercises/morseovka.java
+++ src/String_exercises/morseovka.java
@@ -3,15 +3,28 @@ package String_exercises;
 import java.util.Scanner;
 
 public class morseovka {
-    Scanner sc = new Scanner(System.in);
-    String[] letters = {"A .-", "B -...", "C -.-.", "D -..", "E .",
-            "F ..-.", "G --.", "H ....", "I ..", "J .---",
-            "K -.-", "L .-..", "M --", "N -.", "O ---",
-            "P .--.", "Q --.-", "R .-.", "S ...", "T -",
-            "U ..-", "V ...-", "W .--", "X -..-", "Y -.--",
-            "Z --.."};
-    String input = sc.nextLine();
-    String finalWord = "";
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] letters = {"A .-", "B -...", "C -.-.", "D -..", "E .",
+                "F ..-.", "G --.", "H ....", "I ..", "J .---",
+                "K -.-", "L .-..", "M --", "N -.", "O ---",
+                "P .--.", "Q --.-", "R .-.", "S ...", "T -",
+                "U ..-", "V ...-", "W .--", "X -..-", "Y -.--",
+                "Z --.."};
+        String input = sc.nextLine();
+        input = input.toUpperCase();
+        String finalWord = "";
 
+        for (int i = 0; i < (int)input.length(); i++) {
+            if (input.charAt(i) == letters[i].charAt(0)){   //jestli písmeno na indexu se rovná písmenu v poli
+                for (int j = 2; j < letters[i].length(); j++) {   //zapisuje znaky
+                    finalWord = finalWord + letters[i].charAt(j);
+                }
+                finalWord = finalWord + " ";
+            }
+        }
+
+        System.out.println(finalWord);
+    }
 }
